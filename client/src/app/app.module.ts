f8cb090b726349e9fc7705272ed8cf56133511ea
Innocent Ngene <ngeneinnocent@gmail.com>
2018-02-19 16:23:01 +0100

    route setup

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing-module';
+import * as $ from 'jquery'; //copy into required component.ts and get the ts*type for jquery on line
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    // NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
